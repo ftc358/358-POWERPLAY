286a85543436f8a71f04311d2ec0fee173665919
Kevin Wang <51310353+wangjkevin@users.noreply.github.com>
2022-10-14 15:03:51 -0700

    rename hardware class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
@@ -53,7 +53,7 @@ import com.qualcomm.robotcore.util.Range;
  *
  */
 
-public class MyHardware {
+public class Hardware {
 
     /* Declare OpMode members. */
     private LinearOpMode myOpMode = null;   // gain access to methods in the calling OpMode.
@@ -72,7 +72,7 @@ public class MyHardware {
     public static final double ARM_DOWN_POWER  = -0.45 ;
 
     // Define a constructor that allows the OpMode to pass a reference to itself.
-    public MyHardware(LinearOpMode opmode) {
+    public Hardware(LinearOpMode opmode) {
         myOpMode = opmode;
     }
 
