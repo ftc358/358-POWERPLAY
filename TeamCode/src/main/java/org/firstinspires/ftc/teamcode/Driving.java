6b37b1ff413bad4e808bdce6d44517480f65f3f5
Kevin Wang <51310353+wangjkevin@users.noreply.github.com>
2022-11-01 15:37:52 -0700

    update driving class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Driving.java
+++
@@ -1,8 +0,0 @@
-//package org.firstinspires.ftc.teamcode;
-
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-
-//public class Driving {
-    //public Hardware robot = null;
-
-//}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Driving358.java
@@ -0,0 +1,22 @@
+//package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.Hardware358;
+
+//public class Driving {
+    //public Hardware robot = null;
+
+//}
+
+public class Driving358 {
+    public Hardware358 robot = null;
+
+    public void forward() {
+
+    }
+
+    public void turn() {
+
+    }
+}
